d7960766926992936ed7db49daa64c026fb2e029
RENWENO <66110434+RENWENO@users.noreply.github.com>
2024-09-29 11:40:38 +0800

    Add files via upload

---
+++ DataShart.py
@@ -0,0 +1,47 @@
+#! -*- coding: utf-8 -*-
+# @Time    : 2024/9/22 20:00
+# @Author  : LiuGan
+
+def DataStriping(u2u1,ufu1,uau1,hyper,u2feat,lable,rate):
+    train=[]
+    test=[]
+    keylist1 = []
+    for key1 in u2u1.keys():
+        keylist1.append(key1)
+    num = round(len(keylist1)*rate)
+    trainx=keylist1[:num]
+    testx = keylist1[num:]
+
+    print(train)
+    trainu2u1,trainufu1,trainuau1,trainhyper,trainu2feat,trainlable={},{},{},{},{},{}
+    for tk in trainx:
+        trainu2u1[tk] = u2u1[tk]
+        trainufu1[tk] = ufu1[tk]
+        trainuau1[tk] = uau1[tk]
+        trainhyper[tk]= hyper[tk]
+        trainu2feat[tk] = u2feat[tk]
+        trainlable[tk]  = lable[tk]
+    train.append(trainu2u1)
+    train.append(trainufu1)
+    train.append( trainuau1)
+    train.append(trainhyper)
+    train.append(trainu2feat)
+    train.append(trainlable)
+
+    testu2u1,testufu1,testuau1,testhyper,testu2feat,testlable={},{},{},{},{},{}
+    for ts in testx:
+        testu2u1[ts] = u2u1[ts]
+        testufu1[ts] = ufu1[ts]
+        testuau1[ts] = uau1[ts]
+        testhyper[ts] = hyper[ts]
+        testu2feat[ts] = u2feat[ts]
+        testlable[ts] = lable[ts]
+
+    test.append(testu2u1)
+    test.append(testufu1)
+    test.append(testuau1)
+    test.append(testhyper)
+    test.append(testu2feat)
+    test.append(testlable)
+    return train,test
+
---
+++ Dataload.py
@@ -0,0 +1,148 @@
+#! -*- coding: utf-8 -*-
+# @Time    : 2024/6/19 0:47
+# @Author  : LiuGan
+
+import pickle
+import re
+import numpy as np
+from transformers import BertModel, BertTokenizer
+
+def re1(String):
+    ori =  String
+    cleaned_string1 = re.sub(";anonymized", '', ori)
+    cleaned_string2 = re.sub(";id", '', cleaned_string1)
+    cleaned_string = re.sub(";", ' ', cleaned_string2)
+    return  cleaned_string
+
+#获取属性字典和特征字典,以及属性种类
+with open("../data/Facebook/107.featnames","r",encoding="utf-8") as f4:
+    featdict = {}
+    attributedict = {}
+    allfeatname = f4.readlines()
+    Attributed = set()
+    for allfeat in allfeatname:
+        feat = allfeat.split(" ")
+        num = int(feat[0])
+        Avelue = re1(feat[1])
+        Attributed.add(Avelue)
+        attributedict[num] = Avelue
+        Fvelue = feat[-2]+" "+feat[-1][:-1]
+        featdict[num] = Fvelue
+    Atttiedict={}
+    for i,velu in enumerate(Attributed):
+        Atttiedict[i]=velu
+#
+#
+    print(attributedict)
+    print(featdict)
+    print(Attributed)
+
+#获取所有用户特征
+with open("../data/Facebook/107.feat",'r',encoding="utf-8") as f5:
+    allusefeat = f5.readlines()
+    usefeatdict = {}
+    for usefeat in allusefeat:
+        AttriV = np.zeros((1,23))
+        uf = usefeat[:-1].split(" ")
+        key = uf[0]
+        veul1 = uf[1:]
+        veul = np.array([int(char) for char in uf[1:]])
+        for j,V in enumerate(velu):
+            if V == 1:
+                S=attributedict[j]
+                for k,v in Atttiedict:
+                    if v == S:
+                        key=k
+        #attrie = np.
+        usefeatdict[key]=veul
+    print(usefeatdict)
+
+
+#u-u图
+with open('../data/Facebook/u2u1.pkl', 'rb') as f1:
+    arrays_loaded1 = pickle.load(f1)
+    keylist1 = []
+    for key1 in arrays_loaded1.keys():
+        keylist1.append(key1)
+
+
+#u-A|B-u图
+with open('../data/Facebook/ufu1.pkl', 'rb') as f2:
+    arrays_loaded2 = pickle.load(f2)
+    keylist2 = []
+    for key2 in arrays_loaded2.keys():
+        keylist2.append(key2)
+# def usetofeat(usefrident):
+#     for
+
+with open("../data/Facebook/107.edges","r") as f4:
+    edgs = f4.readlines()
+    friendlist = []
+    friendstrlist =[]
+    alllablelist=[]
+    for u in keylist1:
+        lablelist=[]
+        uF = usefeatdict[u]
+        lable1 = uF[264:266]#2
+        lablelist.append(lable1)
+        lable2 = uF[494:517]#23
+        lablelist.append(lable2)
+        lable3 = uF[220:223]#3
+        lablelist.append(lable3)
+        lable4 = uF[0:17]#16
+        lablelist.append(lable4)
+        usefrident = set()
+        usefridentStr = []
+        for edd in edgs:
+
+            if u== edd[:-1].split(" ")[0]:
+                usefrident.add(edd[:-1].split(" ")[1])
+                F = edd[:-1].split(" ")[1]
+                Farr = usefeatdict[F]
+                indix = np.argwhere(Farr == 1)
+                indix = np.squeeze(indix, axis=1)
+                frientfeatstr = []
+                for id in indix:
+                    featfr = featdict[id]
+                    frientfeatstr.append(featfr)
+                print(frientfeatstr)
+                frientstr = " ".join(frientfeatstr)
+                usefridentStr.append(frientstr)
+
+            # if u == edd[:-1].split(" ")[1]:
+            #     usefrident.add(edd[:-1].split(" ")[0])
+            #     F = edd[:-1].split(" ")[0]
+            #     Farr = usefeatdict[F]
+            #     indix = np.argwhere(Farr == 1)
+            #     indix = np.squeeze(indix, axis=1)
+            #     frientfeatstr = []
+            #     for id in indix:
+            #         featfr = featdict[id]
+            #         frientfeatstr.append(featfr)
+            #     print(frientfeatstr)
+            #     frientstr = " ".join(frientfeatstr)
+            #     usefridentStr.append(frientstr)
+
+
+        friendlist.append(usefrident)
+        friendstrlist.append(usefridentStr)
+        alllablelist.append(lablelist)
+print("aa")
+usefriefeat={}
+selfuselabe = {}
+for i,key in  enumerate(keylist1):
+    velu = friendstrlist[i]
+    velu1 = alllablelist[i]
+    usefriefeat[key]=velu
+    selfuselabe[key] = velu1
+
+    if len(friendstrlist[i]) != arrays_loaded1[key].shape[0]-576:
+        print(key, "朋友个数：", len(friendstrlist[i]))
+        print(key, "朋友个数：", arrays_loaded1[key].shape[0] - 576)
+
+#print(usefeatdict)
+#print("aa")
+with open('labe.pkl','wb') as f7:
+    pickle.dump(selfuselabe,f7)
+with open('u2feat.pkl', 'wb') as f6:
+    pickle.dump(usefriefeat, f6)
